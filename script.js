a50356cebe52932d2b58274dd7d9462ebd941ad1
mcisnado1 <123659112+mcisnado1@users.noreply.github.com>
2023-05-11 08:15:03 -0400

    typeof for undefined list

--- script.js
+++ script.js
@@ -1,108 +1,97 @@
 function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-  
-  function injectHTML(list) {
-    console.log("fired intectHTML");
-    const target = document.querySelector("#restaurant_list");
-    target.innerHTML = "";
-    list.forEach((item, index) => {
-      const str = `<li>${item.name}</li>`;
-      target.innerHTML += str;
-    });
-  }
-  /* A quick filter that will return something based on a matching input */
-  function filterList(list, query) {
-    return list.filter((item) => {
-      const lowerCaseName = item.name.toLowerCase();
-      const lowerCaseQuery = query.toLowerCase();
-      return lowerCaseName.includes(lowerCaseQuery);
-    });
-    /*
-      Using the .filter array method, 
-      return a list that is filtered by comparing the item name in lower case
-      to the query in lower case
-    
-      Ask the TAs if you need help with this
-    */
-  }
-  function cutRestaurantList(list) {
-    console.log("fired cut list");
-    const range = [...Array(15).keys()];
-    return (newArray = range.map((item) => {
-      const index = getRandomIntInclusive(0, list.length - 1);
-      return list[index];
-    }));
-  }
-  
-  async function mainEvent() {
-    // the async keyword means we can make API requests
-    const mainForm = document.querySelector(".main_form"); // This class name needs to be set on your form before you can listen for an event on it
-    const filterDataButton = document.querySelector("#filter");
-    const loadDataButton = document.querySelector("#data_load");
-    const generateListButton = document.querySelector("#generate");
-    const textField = document.querySelector("#school_name");
-    // Add a querySelector that targets your filter button here
-    const loadAnimation = document.querySelector("#data_load_animation");
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
+function injectHTML(list) {
+  console.log("fired injectHTML");
+  const target = document.querySelector("#polling_list");
+  target.innerHTML = "";
+  list.forEach((item) => {
+    const str = `<li>${item.name}</li>`;
+    target.innerHTML += str;
+  });
+}
+
+function filterList(list, query) {
+  return list.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = query.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+  });
+}
+
+function cutPollingPlaceList(list) {
+  console.log("fired cut list");
+  const range = [...Array(20).keys()];
+  return (newArray = range.map((item) => {
+    const index = getRandomIntInclusive(0, list.length - 1);
+    return list[index];
+  }));
+}
+
+async function mainEvent() {
+  const mainForm = document.querySelector(".main_form");
+  const filterButton = document.querySelector("#filter_button");
+  const loadDataButton = document.querySelector("#data_load");
+  const generateListButton = document.querySelector("#generate");
+  const textField = document.querySelector("#place");
+
+  const loadAnimation = document.querySelector("#load_animation");
+  loadAnimation.style.display = "none";
+  generateListButton.classList.add("hidden");
+
+  let currentList = [];
+
+  loadDataButton.addEventListener("click", async (submitEvent) => {
+    console.log("loading data");
+    loadAnimation.style.display = "inline-block";
+    // Basic GET request
+    const results = await fetch(
+      "https://data.princegeorgescountymd.gov/resource/2v6d-7p4w.json"
+    );
+
+    const storedList = await results.json();
+    localStorage.setItem('storedData', JSON.stringify(storedList));
+    if (storedList.length > 0) {
+      generateListButton.classList.remove("hidden");
+    }
+
     loadAnimation.style.display = "none";
-    generateListButton.classList.add("hidden");
-  
-    let storedList = [];
-    let currentList = []; // this is "scoped" to the main event function
-  
-    /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
-    loadDataButton.addEventListener("click", async (submitEvent) => {
-      // async has to be declared on every function that needs to "await" something
-  
-      // this is substituting for a "breakpoint" - it prints to the browser to tell us we successfully submitted the form
-      console.log("Loading data");
-      loadAnimation.style.display = "inline-block";
-  
-      // Basic GET request - this replaces the form Action
-      const results = await fetch(
-        "https://data.princegeorgescountymd.gov/resource/vkdv-rvfx.json"
-      );
-  
-      // This changes the response from the GET into data we can use - an "object"
-      storedList = await results.json();
-      if (storedList.length > 0) {
-        generateListButton.classList.remove("hidden");
-      }
-      loadAnimation.style.display = "none";
-      console.table(storedList);
-      /*injectHTML(currentList);*/
-    });
-  
-    filterDataButton.addEventListener("click", (event) => {
-      console.log("clicked filterButton");
-  
-      const formData = new FormData(mainForm);
-      const formProps = Object.fromEntries(formData);
-  
-      console.log(formProps);
-  
-      const newList = filterList(currentList, formProps.school_name);
-  
-      console.log(newList);
-      injectHTML(newList);
-    });
-  
-    generateListButton.addEventListener("click", (event) => {
-      console.log("generate new list");
-      currentList = cutRestaurantList(storedList);
-      console.log(currentList);
-      injectHTML(currentList);
-    });
-  
-    textField.addEventListener("input", (event) => {
-      console.log("input", event.target.value);
-      const newList = filterList(currentList, event.target.value);
-      console.log(newList);
-      injectHTML(newList);
-    });
-  }
-  
-  document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests
-  
+    //console.table(storedList);
+  });
+
+  filterButton.addEventListener("click", (event) => {
+    console.log("clicked FilterButton");
+
+    const formData = new FormData(mainForm);
+    const formProps = Object.fromEntries(formData);
+
+    console.log(formProps);
+    const newList = filterList(currentList, formProps.place);
+
+    console.log(newList);
+    injectHTML(newList);
+  });
+
+  generateListButton.addEventListener("click", (event) => {
+    console.log("generate new list");
+    const recallList = localStorage.getItem('storedData');
+    console.log('what is the type of recalledList:', typeof recallList);
+    
+    currentList = cutPollingPlaceList(recallList);
+    console.log(recallList);
+    console.log(currentList);
+    injectHTML(currentList);
+  });
+
+  textField.addEventListener("input", (event) => {
+    console.log("input", event.target.value);
+    const newList = filterList(currentList, event.target.value);
+    console.log(newList);
+    injectHTML(newList);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests
